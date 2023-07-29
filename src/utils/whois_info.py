5a44928422a83b1d0eb074b2f1c5f931d0aff9ec
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-07-30 01:08:33 +0300

    The return value of the "get_whois_info" function was converted to JSON format.

--- src/utils/whois_info.py
+++ src/utils/whois_info.py
@@ -1,8 +1,17 @@
 import whois
+import json
+from datetime import datetime
+
+def convert_datetime(obj):
+    if isinstance(obj, datetime):
+        return obj.__str__()
 
 def get_whois_info(domain):
     try:
         whois_info = whois.whois(domain)
-        return whois_info
+        data = whois_info
+        json_data = json.dumps(data, default=convert_datetime)
+
+        return json_data
     except Exception as e:
         return str(e)
