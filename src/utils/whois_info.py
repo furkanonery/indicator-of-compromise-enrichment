a5f34981c18525f336a165bcf447d01514476aee
Furkan Öneryıldız <furkan.oneryildiz@gmail.com>
2023-07-29 16:26:40 +0300

    Changed WHOIS query script.

--- src/utils/whois_info.py
+++ src/utils/whois_info.py
@@ -6,13 +6,3 @@ def get_whois_info(domain):
         return whois_info
     except Exception as e:
         return str(e)
-
-# Test için örnek bir alan adı
-domain_name = "example.com"
-whois_data = get_whois_info(domain_name)
-
-if isinstance(whois_data, dict):
-    for key, value in whois_data.items():
-        print(f"{key}: {value}")
-else:
-    print(f"WHOIS query failed: {whois_data}")
